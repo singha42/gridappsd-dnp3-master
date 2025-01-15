c66c45e5e62a4863e9dd4be1336ac607bc6d9279
singha42 <alka.singh@pnnl.gov>
2025-01-15 08:38:26 -0800

    cleaned up start_service_new

---
+++ dnp3-master/service/start_service_new.py
@@ -0,0 +1,302 @@
+# Copyright (c) 2019 Alliance for Sustainable Energy, LLC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#########################Edited/Prepared this code by Dr.Venkateswara Reddy Motakatla, NREL (VenkateswaraReddy.Motakatla@nrel.gov)
+######################### PNNL Technical support: Poorva, and Alka
+from __future__ import annotations
+
+import argparse
+import csv
+import json
+import logging
+import os
+import platform
+import sys
+import time
+from dataclasses import dataclass
+
+import gridappsd.topics as topics
+import numpy as np
+import yaml
+from dnp3.CIMPro_AIAO_BIBO import CIMProcessor
+
+# from dnp3.master_pnnl import MyMaster, MyLogger, AppChannelListener, SOEHandler, MasterApplication
+from dnp3.dnp3_to_cim import CIMMapping
+from dnp3.points import PointValue
+
+# from dnp3_python.dnp3station.master import MyMaster
+from dnp3_python.dnp3station.master_new import MyMasterNew
+from gridappsd import DifferenceBuilder, GridAPPSD, utils
+from gridappsd.topics import simulation_input_topic, simulation_output_topic
+from pydnp3 import opendnp3, openpal
+
+# Setup argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "outstation_names", nargs="+", help="name of dnp3 outstation", type=str
+)
+parser.add_argument(
+    "--config_path",
+    help="Path of the folder containing input configuration files",
+    default="config",
+    type=str,
+)
+
+# Parse arguments globally
+args = parser.parse_args()
+
+# Now args is available globally within the script
+outstation_names = args.outstation_names
+config_path = args.config_path
+
+
+@dataclass
+class RTUConfig:
+    """
+    A configuration class for RTU (Remote Terminal Unit) devices.
+
+    This class holds all the necessary configuration details for an RTU,
+    allowing easy access and management of its properties. It also includes
+    a static method to load configurations from a JSON file, simplifying
+    initialization and reducing potential setup errors.
+
+    Attributes:
+        name (str): The name of the RTU.
+        conversion_type (str): Type of conversion used by the RTU.
+        CIM_object (str): CIM object identifier.
+        port (int): Network port number the RTU uses for communication.
+        ip (str): IP address of the RTU.
+        desc (str): A brief description of the RTU.
+        link_local_addr (int): Local network address used for linking.
+        link_remote_addr (int): Remote network address used for linking.
+    """
+
+    name: str
+    conversion_type: str
+    CIM_object: str
+    port: int
+    ip: str
+    desc: str
+    link_local_addr: int
+    link_remote_addr: int
+
+    @staticmethod
+    def load_json_config(file_path: str, rtu_key: str) -> RTUConfig:
+        """
+        Load RTU configuration from a JSON file.
+
+        :param file_path: Path to the JSON file containing the RTU configurations.
+        :param rtu_key: Key of the RTU entry to be loaded from the JSON file.
+        :return: An instance of RTUConfig initialized with data from the JSON file.
+
+        # Example usage: Assuming the JSON file is located at 'config.json'
+        # and the desired RTU key is 'RTU1'
+        rtu_config = RTUConfig.load_json_config('config.json', 'RTU1')
+        if rtu_config:
+            print(rtu_config)
+
+        Example config file:
+        {
+            "RTU1": {
+                "name": "RTU1",
+                "conversion_type": "RTU1",
+                "CIM object": "l50",
+                "port": "30000",
+                "ip": "127.0.0.1",
+                "desc": "RTU1",
+                "link_local_addr": "2",
+                "link_remote_addr": "1"
+            },
+            "RTU2": {
+                "name": "RTU2",
+                "conversion_type": "RTU2",
+                "CIM object": "l50",
+                "port": "40000",
+                "ip": "127.0.0.1",
+                "desc": "RTU2",
+                "link_local_addr": "2",
+                "link_remote_addr": "1"
+            }
+        }
+        """
+        try:
+            with open(file_path, "r") as file:
+                data = json.load(file)
+                rtu_data = data[rtu_key]
+                return RTUConfig(
+                    name=rtu_data["name"],
+                    conversion_type=rtu_data["conversion_type"],
+                    CIM_object=rtu_data["CIM object"],
+                    port=int(rtu_data["port"]),
+                    ip=rtu_data["ip"],
+                    desc=rtu_data["desc"],
+                    link_local_addr=int(rtu_data["link_local_addr"]),
+                    link_remote_addr=int(rtu_data["link_remote_addr"]),
+                )
+
+        except (FileNotFoundError, KeyError, json.JSONDecodeError) as e:
+            raise Exception(f"Error loading RTU configuration: {e}")
+
+
+myCIMProcessor = None
+
+logging.basicConfig(level=logging.DEBUG)
+_log = logging.getLogger(__name__)
+
+
+def on_message(headers, message):
+    """
+    A callback function when the receiving message from a subscribed topic.
+    """
+    myCIMProcessor.process(message)
+
+
+def _register_mapping(register_name: str, db_data):
+    """# mapping based on register name, e.g., ufls_59.1" -> "BinaryOutputStatus"[0]"""
+    if register_name == "ufls_59.1":
+        return db_data["BinaryOutputStatus"][0]
+    else:
+        return db_data["BinaryOutputStatus"][1]
+
+
+def _construct_cim_full_msg(
+    db_data,
+    cim_full_msg=None,
+) -> dict:
+    """
+    Constructs a CIM message with measurement data.
+    Args:
+        cim_full_msg (dict, optional): Initial structure of the CIM message. Defaults to None.
+    Returns:
+        dict: A dictionary containing the CIM message structured with measurements.
+    Example message:
+    {
+        "simulation_id" : "12ae2345",
+        "message" : {
+            "timestamp" : "1357048800",
+            "measurements" : {
+                "123a456b-789c-012d-345e-678f901a234b":{
+                                    "measurement_mrid" : "123a456b-789c-012d-345e-678f901a234b",
+                                    "value": True  # enable is True, disable is False
+                                    # "magnitude" : 3410.456,
+                                    # "angle" : -123.456
+                                    }
+                            }
+                    }
+    }
+    """
+    if cim_full_msg is None:
+        cim_full_msg = {"message": {}}
+
+    # Load measurement definitions from a JSON file.
+    # config_path = "path_to_config"  # Define or import the configuration path.
+    with open(os.path.join(config_path, "new_measurement_dict_master.json")) as f:
+        new_measurement_dict_master = json.load(f)
+
+    # Populate the CIM message with the latest measurements.
+    for key, value in new_measurement_dict_master.items():
+        mr_id = value["Pos"]["ABC"][0]["mrid"]
+        # Assuming _register_mapping and db_data are defined elsewhere
+        value = (
+            True if _register_mapping(key, db_data) else False
+        )  # Note: db_data can be None
+        current_time = int(time.time())
+        cim_full_msg["message"]["timestamp"] = str(current_time)
+        cim_full_msg["message"]["measurements"] = {
+            mr_id: {"measurement_mrid": mr_id, "value": value}
+        }
+        # Log the prepared message
+        _log.debug(f"Publishing CIM measurement XXXX: {json.dumps(cim_full_msg)}")
+
+    return cim_full_msg
+
+
+def run_master():
+    # init master station(s)
+    config_path = args.config_path
+    _log.debug(f"{config_path = }")
+    master_apps = {}
+    for outstaion_name in outstation_names:
+        config: RTUConfig = RTUConfig.load_json_config(
+            os.path.join(config_path, "device_ip_port_config.json"), outstaion_name
+        )
+        master_app = MyMasterNew(
+            master_ip="0.0.0.0",
+            outstation_ip=config.ip,
+            port=config.port,
+            master_id=config.link_local_addr,
+            outstation_id=config.link_remote_addr,
+        )
+        master_app.start()
+        master_apps[outstaion_name] = master_app
+
+    # loop to poll data from outstation, construct a cim-message and send it
+    msg_count = 0
+    while True:
+        for outstation_name, master_app in master_apps.items():
+            # Send scan request and process data
+            master_app.send_scan_all_request()
+            db_data = master_app.soe_handler.db
+
+            try:
+                cim_full_msg = _construct_cim_full_msg(db_data)
+            except Exception as e:
+                _log.error(f"Failed to construct CIM message: {e}")
+                continue  # Continue with the next iteration if an error occurs.
+
+            # Sending the cim-message
+            # _log.info(f"Sending CIM message: {cim_full_msg}")
+            gapps.send(
+                topics.field_output_topic(), json.dumps(cim_full_msg)
+            )  # TODO: confirm what the topic to use
+
+        msg_count += 1
+        time.sleep(2)  # Wait for 2 seconds before the next cycle
+
+
+if __name__ == "__main__":
+    # TODO: Change dummy simulation id to field id
+    # simulation_id = "field_data"
+    # gapps = GridAPPSD(stomp_address="10.15.223.157", stomp_port=61613)
+    gapps = GridAPPSD()
+    gapps.connect()
+
+    with open(config_path + "/device_ip_port_config.json") as f:
+        device_ip_port_config_all_Xcel = json.load(f)
+
+    dnp3_to_cim = CIMMapping(
+        conversion_dict=os.path.join(config_path, "conversion_dict_master_data.json"),
+        model_line_dict=os.path.join(config_path, "measurement_dict_master.json"),
+    )
+
+    conversion_dict = dnp3_to_cim.conversion_dict
+    # print(conversion_dict)
+
+    time.sleep(1)
+
+    run_master()
